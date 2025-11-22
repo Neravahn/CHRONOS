e3d37a75c78a5672b2796004ab6b48fe03345aef
Neravahn <prashantc48774.r@gmail.com>
2025-11-22 20:03:05 +0530

    connected everything

--- static/js/range.js
+++ static/js/range.js
@@ -1,6 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
 const velSlider = document.querySelector('.velocity');
 const velText = document.getElementById('vel_range');
+const upBtn = document.getElementById('up');
+const downBtn = document.getElementById('down');
+const stepsInput = document.getElementById('steps');
+
+
+upBtn.addEventListener('click', () => {
+    let step = parseFloat(stepsInput.value) || 0.01;
+    let newValue = parseFloat(velSlider.value) + step;
+    if (newValue > parseFloat(velSlider.max)) {
+        newValue = parseFloat(velSlider.max);
+    }
+
+    velSlider.value = newValue;
+    velSlider.dispatchEvent(new Event ('input'));
+});
+
+downBtn.addEventListener('click', () => {
+    let step = parseFloat(stepsInput.value) || 0.01;
+    let newValue = parseFloat(velSlider.value) - step;
+    if (newValue < parseFloat(velSlider.min)) {
+        newValue = parseFloat(velSlider.min);
+    }
+
+    velSlider.value = newValue;
+    velSlider.dispatchEvent(new Event ('input'));
+});
 
 velText.textContent =velSlider.value + '% of C';
 velSlider.addEventListener('input', () => {
