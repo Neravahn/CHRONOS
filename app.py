611cb91cb641479eb079c5ec1d11587f7dbb0a32
Neravahn <prashantc48774.r@gmail.com>
2025-11-22 15:51:59 +0530

    added basic structure in the canvas area

--- app.py
+++ app.py
@@ -10,20 +10,16 @@ def index():
 
 @app.route('/velocity_simulation', methods=['GET'])
 def velocity_simulation():
-    return render_template('index.html')
+    return render_template('velocity_simulation.html')
 
 @app.route('/gravity_simulation', methods=['GET'])
 def gravity_simulation():
-    return render_template('index.html')
+    return render_template('gravity_simulation.html')
 
 
 @app.route('/twin_paradox', methods=['GET'])
 def twin_paradox():
-    return render_template('index.html')
+    return render_template('twin_paradox.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-
-
-
