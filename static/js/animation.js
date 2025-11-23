10ae0c07ca34d059daf1a33cc5b7b90e6d269188
Neravahn <prashantc48774.r@gmail.com>
2025-11-23 20:32:44 +0530

    added zoom and clamp

--- static/js/animation.js
+++ static/js/animation.js
@@ -2,6 +2,7 @@ import { getGamma } from './physics.js';
 import { drawDigitalClock } from './clock.js';
 import { drawSpaceship } from './movingObject.js';
 import { initStars, drawStars } from './starfield.js'
+import { camera, setupCameraControls, applyCameraTransform } from './camera.js'
 
 
 export function startAnimation(canvasID, getVelocity) {
@@ -13,19 +14,23 @@ export function startAnimation(canvasID, getVelocity) {
     let startTime = Date.now();
 
     initStars(300, canvas.width, canvas.height);
+    setupCameraControls(canvas);
 
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         const velocity = getVelocity();
-        const gamma = Math.max(getGamma(velocity), 1);;
+        const gamma = Math.max(getGamma(velocity), 1);
+        let now = Date.now();
+        let realElapsed = now - startTime;
+        let dilatedElapsed = realElapsed / gamma;
+
+        ctx.save();
+        applyCameraTransform(ctx);
 
         //STARS
         drawStars(ctx, canvas.width, canvas.height, velocity);
 
-        let now = Date.now();
-        let realElapsed = now - startTime;
-        let dilatedElapsed = realElapsed / gamma;
 
         const paddingBetweenClocks = 200;
         const clockWidth = 200;
@@ -34,21 +39,24 @@ export function startAnimation(canvasID, getVelocity) {
         const startX = (canvas.width - totalWidth) / 2;
         const position = canvas.height / 10;
 
-        const firstClockX = startX + clockWidth / 2;
-        const secondClcokx = firstClockX + clockWidth + paddingBetweenClocks;
-
-
-
-        drawDigitalClock(ctx, firstClockX, position, clockWidth, clockHeight, realElapsed, 'Real Time');
-        drawDigitalClock(ctx, secondClcokx, position, clockWidth, clockHeight, dilatedElapsed, 'Dilated Time');
 
         //SPACESHIP
         const spaceshipY = position + clockHeight + 400;
         const spaceshipWidth = 1000;
         const spaceshipHeight = 200;
-
         drawSpaceship(ctx, canvas.width / 2, spaceshipY, spaceshipWidth, spaceshipHeight, gamma);
+        ctx.restore();
+
+
+
+        //CLOCK AREA
+        const firstClockX = startX + clockWidth / 2;
+        const secondClcokx = firstClockX + clockWidth + paddingBetweenClocks;
+        drawDigitalClock(ctx, firstClockX, position, clockWidth, clockHeight, realElapsed, 'Real Time');
+        drawDigitalClock(ctx, secondClcokx, position, clockWidth, clockHeight, dilatedElapsed, 'Dilated Time');
+
         requestAnimationFrame(animate);
+        
     }
 
     animate();
---
+++ static/js/camera.js
@@ -0,0 +1,109 @@
+export const camera = {
+    x: 0,
+    y: 0,
+    zoom: 1
+};
+
+
+let isPanning = false;
+let startx = 0;
+let starty = 0;
+
+//LIMITS
+const min_zoom = 0.7;
+const max_zoom = 1.2;
+
+//BOUNDARIES
+const world = {
+    left: -2000,
+    right: 2000,
+    top: -2000,
+    bottom: 2000
+};
+
+function clampCamera(canvas) {
+    const viewH = canvas.height / camera.zoom;
+    const viewW = canvas.width / camera.zoom;
+
+    const halfWorldW = (world.right - world.left) /2;
+    const halfWorldH = (world.bottom - world.top) / 2;
+
+    const maxx = halfWorldW - viewW / 2;
+    const minx = -maxx;
+
+    const maxy = halfWorldH - viewH / 2;
+    const miny = -maxy;
+
+    if (viewH >= halfWorldH * 2) {
+        camera.y = 0;
+    } else {
+        camera.y = Math.max(miny, Math.min(maxy, camera.y));
+    }
+
+    if (viewW >= halfWorldW * 2) {
+        camera.x = 0;
+    } else {
+        camera.x = Math.max(minx, Math.min(maxx , camera.x));
+    }
+
+    
+}
+
+export function setupCameraControls(canvas) {
+    canvas.addEventListener('mousedown', (e) => {
+        isPanning = true;
+        startx = e.clientX;
+        starty = e.clientY;
+    });
+
+    canvas.addEventListener('mousemove', (e) => {
+        if (!isPanning) {
+            return;
+        }
+
+        const distancex = (e.clientX - startx) / camera.zoom;
+        const distancey = (e.clientY - starty) / camera.zoom;
+
+        camera.x += distancex;
+        camera.y += distancey;
+
+        clampCamera(canvas);
+
+        startx = e.clientX;
+        starty = e.clientY;
+    });
+
+    canvas.addEventListener('mouseup', () => {
+        isPanning = false;
+    });
+
+    canvas.addEventListener('mouseleave', () => {
+        isPanning = false;
+    });
+
+    canvas.addEventListener('wheel', (e) => {
+        e.preventDefault();
+
+        const rect = canvas.getBoundingClientRect();
+        const mousex = (e.clientX - rect.left) / camera.zoom - camera.x;
+        const mousey = (e.clientY - rect.top) / camera.zoom - camera.y;
+
+        const zoomIntensity = 0.001;
+        const oldZoom = camera.zoom;
+
+        camera.zoom -= e.deltaY * zoomIntensity;
+        camera.zoom = Math.max(min_zoom, Math.min(camera.zoom, max_zoom));
+
+        const zoomRatio = camera.zoom / oldZoom;
+
+        camera.x -= mousex * (zoomRatio - 1);
+        camera.y -= mousey * (zoomRatio - 1);
+
+        clampCamera(canvas);
+    });
+}
+
+export function applyCameraTransform(ctx) {
+    ctx.scale(camera.zoom, camera.zoom);
+    ctx.translate(camera.x, camera.y);
+}
